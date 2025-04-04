ac1d3593b8b394842da701a089b0822c88e1e201
Tsmith04 <tristen.smith@nxtm.com>
2025-04-04 00:23:21 -0500

    Add  initial header file definitions for basic blocks of the system

---
+++ Include/Sensor.h
@@ -0,0 +1,11 @@
+#include "SPI.h"
+#include "BTLE.h"
+
+/*
+ *
+ * File Name: Sensor.h
+ * Author: Tristen Smith
+ * Date: 04/03/2025
+ * Description: Header file for the Bluetooth Peripheral Sensor onject and necessary functions
+ * Contains the Sensor Class definitions for a sensor and selection logic based on sensor ID
+*/
